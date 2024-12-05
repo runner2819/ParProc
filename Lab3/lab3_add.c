36cbb604c393a3ceea3cef0c63442df1ed916748
Maxim <tokaev.maxim@gmail.com>
2024-12-05 16:23:47 +0300

    lab 3 done? (i think so, but who knows)

--- Lab3/lab3_add.c
+++ Lab3/lab3_add.c
@@ -41,65 +41,90 @@ int main(int argc, char **argv) {
 //        arrays[seed_num] = malloc((int) 1e7 * sizeof(int));
 //        generate_array(arrays[seed_num], 1e7, 920214 + seed_num * 1024);
 //    }
-
-    for (int count = 5 * 1e5; count <= 1e7; count += 5 * 1e5) {
-        double test_start = omp_get_wtime();
+    int count = 1e8;
+//    for (int count = 5 * 1e5; count <= 1e7; count += 5 * 1e5) {
+    double test_start = omp_get_wtime();
 //        const int count = pow(10, 5);     ///< Number of array elements
-        const int random_seed = 920214; ///< RNG seed
-        const int threads = 8;  /// to avoid E-cores on my machine
-        const int num_seed = 30;
+    const int random_seed = 920214; ///< RNG seed
+    const int threads = 8;  /// to avoid E-cores on my machine
+//        const int num_seed = 30;
+    const int num_seed = 0;
 
-        int *gaps = malloc(((int) log2(count) + 10) * sizeof(int));
-        int s_gap = 0;
-        for (int step = count / 2; step > 0; step /= 2) {
-            gaps[s_gap] = step;
-            s_gap++;
-        }
-        gaps = realloc(gaps, s_gap * sizeof(int));
 
-        int *array = malloc(count * sizeof(int));
-        int *clone = malloc(count * sizeof(int));
-        double *s = calloc(omp_get_num_procs(), sizeof(double));
+    int *gaps = malloc(((int) log2(count) + 10) * sizeof(int));
+    int s_gap = 0;
+    for (int step = count / 2; step > 0; step /= 2) {
+        gaps[s_gap] = step;
+        s_gap++;
+    }
+    gaps = realloc(gaps, s_gap * sizeof(int));
 
-        for (int seed_num = 0; seed_num < num_seed; seed_num++) {
+    int *array = malloc(count * sizeof(int));
+    int *clone = malloc(count * sizeof(int));
+    double *s = calloc(omp_get_num_procs(), sizeof(double));
+
+    for (int seed_num = 0; seed_num < num_seed; seed_num++) {
 //        printf("-----------------\n");
 //        printf("Seed %d\n", seed_num);
 
 //        double st = omp_get_wtime();
-            generate_array(array, count, random_seed + seed_num * 1024);
+        generate_array(array, count, random_seed + seed_num * 1024);
 //            memcpy(array, arrays[seed_num], count * sizeof(int));
-            memcpy(clone, array, count * sizeof(int));
+        memcpy(clone, array, count * sizeof(int));
 //        printf("time to generate %lf\n", omp_get_wtime() - st);
-            s[0] += sort(array, count, 1, gaps, s_gap);
+        s[0] += sort(array, count, 1, gaps, s_gap);
 //            memcpy(array, arrays[seed_num], count * sizeof(int));
-            memcpy(array, clone, count * sizeof(int));
-            s[7] += sort(array, count, 8, gaps, s_gap);
+        memcpy(array, clone, count * sizeof(int));
+        s[7] += sort(array, count, 8, gaps, s_gap);
 //        for (int n = 1; n <= threads; n++) {
 //            s[n - 1] += sort(array, count, n, gaps, s_gap);
 //            memcpy(array, clone, count * sizeof(int));
 //        }
-        }
-        double test_end = omp_get_wtime();
-        free(array);
-        free(clone);
-        free(gaps);
-
-        printf("\nCount: %d\nNum_seed: %d\n", count, num_seed);
-        printf("Total time:%lf\n", test_end - test_start);
-        printf("Time:\n");
-        for (int i = 1; i <= threads; i++) {
-            printf("(%d,%lf)", i, s[i - 1] / num_seed);
-        }
-        printf("\nAcceleration:\n");
-        for (int i = 1; i <= threads; i++) {
-            printf("(%d,%lf)", i, s[0] / s[i - 1]);
-        }
-        printf("\nEfficiency:\n");
-        for (int i = 1; i <= threads; i++) {
-            printf("(%d,%lf)", i, s[0] / s[i - 1] / i);
-        }
-//        printf("\n(%d,%lf)\n", count, s[0] / s[7] / 8);
-        free(s);
     }
+    double test_end = omp_get_wtime();
+    free(array);
+    free(clone);
+//    free(gaps);
+
+    printf("\nCount: %d\nNum_seed: %d\n", count, num_seed);
+    printf("Total time:%lf\n", test_end - test_start);
+    printf("Time:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[i - 1] / num_seed);
+    }
+    printf("\nEff:\n");
+    for (int i = 1; i <= threads; i++) {
+        double sum = 0;
+        for (int j = 0; j < s_gap; j++) {
+            sum += (double) 1 / gaps[j];
+        }
+        double sum2 = 0;
+        for (int j = 0; j < s_gap; j++) {
+            if (gaps[j] < i) break;
+            sum2 += (double) 1 / gaps[j];
+        }
+        sum2 /= i;
+        double sum3 = 0;
+        for (int j = s_gap - 1; j > -1; j--) {
+            if (gaps[j] >= i) break;
+            sum3 += (double) 1 / gaps[j] / (i-gaps[j]);
+        }
+        printf("(%d,%lf)", i, sum / (sum2 + sum3)/i);
+    }
+    printf("\nTheory Acceleration:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[0] / s[i - 1]);
+    }
+
+    printf("\nEfficiency:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[0] / s[i - 1] / i);
+    }
+
+//        printf("\n(%d,%lf)\n", count, s[0] / s[7] / 8);
+    free(s);
+    free(gaps);
+//}
+
     return 0;
 }
