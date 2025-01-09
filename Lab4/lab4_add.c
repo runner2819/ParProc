3c2ccc22a4150f75bfd3b592d41ac928a8dd8dc8
Maxim <tokaev.maxim@gmail.com>
2025-01-09 17:56:20 +0300

    started work on qoi

--- Lab4/lab4_add.c
+++ Lab4/lab4_add.c
@@ -20,7 +20,7 @@ double time_count_factors(const long long start, const long long end, const long
     double start_time = omp_get_wtime();
 #pragma omp parallel num_threads(threads) shared(start, end, num_of_factors) default(none)
     {
-#pragma omp for schedule(dynamic, 1)
+#pragma omp for schedule(runtime)
         for (long long i = start; i <= end; i++) {
             num_of_factors[i - start] = count_factors(i);
         }
@@ -33,13 +33,15 @@ double time_count_factors(const long long start, const long long end, const long
 int main() {
     double test_start = omp_get_wtime();
     const long long threads = omp_get_num_procs();  /// to avoid E-cores on my machine
-    const int tries = 10;
+    const int tries = 1;
 
-    long long start = 1, end = 1e6;
+    long long start = 1, end = 1e5;
+    omp_set_schedule(omp_sched_dynamic, 1);
+
     double *s = calloc(omp_get_num_procs(), sizeof(double));
 
     for (int i = 0; i < tries; i++) {
-        printf("%d\n", i);
+//        printf("%d\n", i);
         for (int n = 1; n <= threads; n++) {
             s[n - 1] += time_count_factors(start, end, n);
         }
@@ -62,6 +64,62 @@ int main() {
     }
     printf("\n%lf", s[0] / s[threads - 1] / threads);
     free(s);
+//
+    omp_set_schedule(omp_sched_dynamic, 1000);
+    s = calloc(omp_get_num_procs(), sizeof(double));
 
+    for (int i = 0; i < tries; i++) {
+//        printf("%d\n", i);
+        for (int n = 1; n <= threads; n++) {
+            s[n - 1] += time_count_factors(start, end, n);
+        }
+    }
+
+    test_end = omp_get_wtime();
+
+    printf("Total time:%lf\n", test_end - test_start);
+    printf("Time:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[i - 1] / tries);
+    }
+    printf("\nAcceleration:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[0] / s[i - 1]);
+    }
+    printf("\nEfficiency:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[0] / s[i - 1] / i);
+    }
+    printf("\n%lf", s[0] / s[threads - 1] / threads);
+    free(s);
+    //
+    omp_set_schedule(omp_sched_dynamic, 10000);
+
+    s = calloc(omp_get_num_procs(), sizeof(double));
+
+    for (int i = 0; i < tries; i++) {
+//        printf("%d\n", i);
+        for (int n = 1; n <= threads; n++) {
+            s[n - 1] += time_count_factors(start, end, n);
+        }
+    }
+
+    test_end = omp_get_wtime();
+
+    printf("Total time:%lf\n", test_end - test_start);
+    printf("Time:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[i - 1] / tries);
+    }
+    printf("\nAcceleration:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[0] / s[i - 1]);
+    }
+    printf("\nEfficiency:\n");
+    for (int i = 1; i <= threads; i++) {
+        printf("(%d,%lf)", i, s[0] / s[i - 1] / i);
+    }
+    printf("\n%lf", s[0] / s[threads - 1] / threads);
+    free(s);
     return 0;
 }
